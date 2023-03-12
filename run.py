b2ed04503c3865eb4b724c1c88e2441c6df9231d
Brunna <brunnacroches@gmail.com>
2023-03-12 20:15:27 -0300

    class composition

---
+++ run.py
@@ -0,0 +1,4 @@
+from repository import Repository
+
+repo = Repository()
+repo.select_by_id()
---
+++ sql_actions/select.py
@@ -0,0 +1,6 @@
+class Select:
+    def select_many(self):
+        print('Select Many')
+    
+    def select_one(self):
+        print('Select One')
